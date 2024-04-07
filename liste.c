1849e7517e19749fb28442696a42580daaf19961
Antonio <antoniodragne35@gmail.com>
2024-04-07 17:49:02 +0300

    liste.c

--- liste.c
+++ liste.c
@@ -1,9 +1,20 @@
 #include "liste.h"
 
 
+float tPoints(Echipa team, int nr_part){
+	float points;
+	int k = 0;
+	int i;
+	for(i = 0; i<nr_part; i++){
+		k = team.jucator[i].points + k;
+	}
+	points = k/nr_part;
+	return points;
+}
+
 void addTeamAtBeginning(Node** head, Echipa v){
 	Node* newNode = (Node*)malloc(sizeof(Node));
-    strcpy(newNode->val.nume_echipa ,v.nume_echipa);
+    newNode->val.nume_echipa = strdup(v.nume_echipa); //folosim functia strdup pt a aloca memorie si pt a copia continutul
     newNode->next = *head;
     *head = newNode;
 }
@@ -12,7 +23,7 @@ void addTeamAtBeginning(Node** head, Echipa v){
 void addTeamAtEnd(Node** head, Echipa v){
 	Node *aux = *head;
 	Node *newNode = (Node*)malloc(sizeof(Node)); 
-	strcpy(newNode->val.nume_echipa ,v.nume_echipa);
+	newNode->val.nume_echipa = strdup(v.nume_echipa);
 	if (*head == NULL) addTeamAtBeginning(head, v);
 	else{ 
 		while (aux->next!=NULL) 
@@ -22,23 +33,29 @@ void addTeamAtEnd(Node** head, Echipa v){
 	}
 }
 
-// void deleteNode(Node **head, Echipa v){	
-// 	if (*head==NULL) return; 
-// 	Node *headcopy = *head;
-// 	if (headcopy->val == v){
-// 		*head = (*head)->next;
-// 		free(headcopy);
-// 		return; 
-// 	}
-// 	Node *prev = *head;
-// 	while (headcopy!=NULL){
-// 		if(headcopy->val!=v){
-// 			prev = headcopy;
-// 			headcopy = headcopy->next;
-// 		}else{
-// 			prev->next = headcopy->next;
-// 			free(headcopy); 
-// 			return; 
-// 		} 
-// 	}
-// }
+int deleteTeam(Node **head, Echipa v, int nr_echipe, int nr_min_points){	
+	if (*head==NULL){
+	nr_echipe--;
+	return nr_echipe;
+	} 
+	Node *headcopy = *head;
+	if (headcopy->val.teamPoints == v.teamPoints){
+		*head = (*head)->next;
+		free(headcopy);
+		nr_echipe--;
+		return nr_echipe; 
+	}
+	Node *prev = *head;
+	while (headcopy!=NULL){
+		if(headcopy->val.teamPoints != nr_min_points){
+			prev = headcopy;
+			headcopy = headcopy->next;
+		}else if(headcopy->val.teamPoints == nr_min_points){
+			prev->next = headcopy->next;
+			free(headcopy); 
+			nr_echipe--;
+			return nr_echipe; 
+		} 
+	}
+	return nr_echipe;
+}
