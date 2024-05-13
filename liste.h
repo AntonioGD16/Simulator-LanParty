70b7649e43ae02d4a8ed748ac200dab1cf06e2a0
Antonio <antoniodragne35@gmail.com>
2024-05-13 19:32:49 +0300

    Final

--- liste.h
+++ liste.h
@@ -1,27 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-typedef struct{
-        char* nume;
-        char* prenume;
-        int points;
-}player;
-
-typedef struct team{
-        char* nume_echipa;
-        int nr_part;
-        float teamPoints;
-        player* jucator;
-}Echipa;
-
-typedef struct Elem{
-        Echipa val;
-        struct Elem* next;
-}Node;
+#include "struct.h"
 
 
 void addTeamAtBeginning(Node** , Echipa );
 void addTeamAtEnd(Node** ,  Echipa );
 float tPoints(Echipa , int );
-int deleteTeam(Node **, Echipa , int , int);
+int deleteTeam(Node **, int , float);
+Node* createTeamList(FILE* , FILE*, int*);
+void task1(FILE*, FILE*);
+void task2(FILE*, FILE*);
